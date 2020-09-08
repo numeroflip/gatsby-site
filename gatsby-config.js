5b5a9ad7f11808c4dd26ad99ef972dc4d08a17e1
numeroflip <berenyi.aron@hotmail.hu>
2020-09-08 22:20:22 +0200

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,11 +12,18 @@ module.exports = {
 
   plugins: [
     {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'src',
         path: `${__dirname}/src/`
-      }
+       }
     },
     'gatsby-plugin-sharp',
     {
